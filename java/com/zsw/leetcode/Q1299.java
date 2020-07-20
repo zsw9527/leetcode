ba44d1dbec1613db20f148b2952e894c54fe1440
zsw9527 <zsw1105213969@163.com>
2020-07-20 21:45:34 +0800

    Q1299优化解法

--- java/com/zsw/leetcode/Q1299.java
+++ java/com/zsw/leetcode/Q1299.java
@@ -3,18 +3,15 @@ package com.zsw.leetcode;
 public class Q1299 {
 	
 	public int[] replaceElements(int[] arr) {
+		/**
+		 * 逆序遍历
+		 */
 		int len = arr.length;
-		for (int i = 0; i < len - 1; ++i) {
-			int j = i+1;
-			int max = arr[j];
-			for (; j < len; ++j) {
-				if (arr[j] > max) {
-					max = arr[j];
-				}
-			}
-			arr[i] = max;
+		int[] res = new int[len];
+		res[len - 1] = -1;
+		for (int i = len - 2; i >= 0; i--) {
+			res[i] = Math.max(res[i+1], arr[i+1]);
 		}
-		arr[len-1] = -1;
-		return arr;
+		return res;
     }
 }
