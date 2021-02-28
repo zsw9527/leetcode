017f476791c5ffdec8daeb9f5119bf53cb695994
zsw9527 <zsw1105213969@163.com>
2021-02-28 10:41:48 +0800

    Q3

--- java/com/zsw/leetcode/Q3.java
+++ java/com/zsw/leetcode/Q3.java
@@ -1,21 +1,43 @@
 package com.zsw.leetcode;
 
 import java.util.Deque;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 public class Q3 {
 
     public int lengthOfLongestSubstring(String s) {
-        Deque<Character> que = new LinkedList<>();
-        int res = 0;
-        for (int i = 0, j = 0; i < s.length(); ++i) {
-            while (que.contains(s.charAt(i))) {
-                que.pop();
-                j++;
+        Map<Character, Integer> table = new HashMap<>();
+        int left = 0, right = 0;
+        int len = Integer.MIN_VALUE;
+        while (right < s.length()) {
+            char ch = s.charAt(right);
+            right++;
+            table.put(ch, table.getOrDefault(ch, 0) + 1);
+
+            if (table.get(ch) > 1) {
+                while (left < right) {
+                    if (s.charAt(left) == ch) {
+                        table.put(ch, 1);
+                        break;
+                    } else {
+                        table.remove(s.charAt(left));
+                    }
+                    left++;
+                }
+                left++;
             }
-            que.offer(s.charAt(i));
-            res = Math.max(res, i - j + 1);
+            if (right - left > len) {
+                len = right - left;
+            }
         }
-        return res;
+
+        return len == Integer.MIN_VALUE ? 0 : len;
     }
+
+    public static void main(String[] args) {
+        new Q3().lengthOfLongestSubstring("tmmzuxt");
+    }
+
 }
