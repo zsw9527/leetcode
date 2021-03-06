99cb15a3ea11202cd3abab39ec5554ca15cb6b98
zsw9527 <zsw1105213969@163.com>
2021-03-06 22:41:29 +0800

    岛屿的周长

--- java/com/zsw/leetcode/Q200.java
+++ java/com/zsw/leetcode/Q200.java
@@ -1,5 +1,37 @@
 package com.zsw.leetcode;
 
+
+/**
+ * 岛屿数量
+ * @author zsw
+ * @date 2021/3/6 21:51
+ */
 public class Q200 {
 
+	public int numIslands(char[][] grid) {
+		int res = 0;
+		
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length; j++) {
+				if (grid[i][j] == '1') {
+                    res++;
+					bfs(grid, i, j);
+				}
+			}
+		}
+		
+		return res;
+	}
+	
+	private void bfs(char[][] grid, int i, int j) {
+		if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
+			return;
+		}
+		
+		grid[i][j] = '0';
+		bfs(grid, i + 1, j);
+		bfs(grid, i, j + 1);
+		bfs(grid, i - 1, j);
+		bfs(grid, i, j - 1);
+	}
 }
